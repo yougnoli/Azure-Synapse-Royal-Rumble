23908d69f7255e77b75a6cabac64d9a81294f941
Alessio Tugnoli <77077281+yougnoli@users.noreply.github.com>
2022-09-08 18:37:04 +0200

    Create data-exploration-database.sql

---
+++ 01/data-exploration-database.sql
@@ -0,0 +1,8 @@
+/*
+You can browse the content of the files directly via *master* database. 
+For some simple data exploration scenarios, you don't need to create a separate database. However, as you continue data exploration, you might want to create some utility objects, such as:
+- **External data sources** that represent the named references for storage accounts.
+- **Database scoped credentials** that enable you to specify how to authenticate to external data source.
+- **Database users** with the permissions to access some data sources or database objects.
+- Utility **views**, **procedures**, and **functions** that you can use in the queries.
+*/
